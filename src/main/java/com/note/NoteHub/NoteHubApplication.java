34b23d014e63fad044fd2815c51854b9839f0246
Angelina <motidzuki.yasu@gmail.com>
2024-06-17 11:05:15 +0300

    Initial commit

---
+++ src/main/java/com/note/NoteHub/NoteHubApplication.java
@@ -0,0 +1,13 @@
+package com.note.NoteHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteHubApplication.class, args);
+	}
+
+}
