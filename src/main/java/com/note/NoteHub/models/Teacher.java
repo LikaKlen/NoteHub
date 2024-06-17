7b84068c2ac8b470a4444bdefb4ad244a93b617a
Inna <morozovainna71@gmail.com>
2024-06-17 13:35:33 +0300

    что-нибудь

--- src/main/java/com/note/NoteHub/models/Teacher.java
+++ src/main/java/com/note/NoteHub/models/Teacher.java
@@ -1,4 +1,28 @@
 package com.note.NoteHub.models;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@Table(name = "teacher", schema = "public", catalog = "NoteHub")
+@NoArgsConstructor
 public class Teacher {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long teacherId;
+    @Column(name = "firstname")
+    private String firstName;
+    @Column(name = "lastname")
+    private String lastName;
+    @Column(name = "patronymic")//отчество
+    private String patronymic;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "email")
+    private String email;
+
+
 }
