463b30feac3e4ecf0471486bd1a9f88622ea5227
Angelina <motidzuki.yasu@gmail.com>
2024-06-18 12:02:39 +0300

    comi

--- src/main/java/com/note/NoteHub/security/JwtCore.java
+++ src/main/java/com/note/NoteHub/security/JwtCore.java
@@ -1,4 +1,24 @@
 package com.note.NoteHub.security;
-
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+@Component
 public class JwtCore {
+    @Value("${NoteHub.app.secret}")
+    private String secret;
+    @Value("${NoteHub.app.lifetime}")
+    private int lifetime;
+    public String generateToken(UserDetails userDetails){
+        return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
+                .setExpiration(new Date((new Date().getTime() + lifetime)))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+    public String getNameFromJwt(String token) {
+        return Jwts.parser().setSigningKey(secret).build().parseClaimsJws(token).getBody().getSubject();
+    }
 }
+
