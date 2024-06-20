5a18f310ba1179aa44c64be4f2cdf380f9b4ad22
Inna <morozovainna71@gmail.com>
2024-06-20 14:30:55 +0300

    finale touch 0.3v

--- src/main/java/com/note/NoteHub/control/MainController.java
+++ src/main/java/com/note/NoteHub/control/MainController.java
@@ -20,72 +20,72 @@ public class MainController {
     }
 
     //Group
-    @GetMapping("/get/group/{id}")
+    @GetMapping("group/{id}")
     public ResponseEntity getGroupById(@PathVariable("id") long id){
         return ResponseEntity.ok(dataAccessLayer.getGroupById(id));
     }
-    @GetMapping("get/groups")
+    @GetMapping("groups")
     public ResponseEntity getGroups(){
         return ResponseEntity.ok(dataAccessLayer.getGroups());
     }
     //Parent
-    @GetMapping("/get/parent/{id}")
+    @GetMapping("parent/{id}")
     public ResponseEntity getParentById(@PathVariable("id") long id){
         return ResponseEntity.ok(dataAccessLayer.getParentById(id));
     }
-    @GetMapping("get/parents")
+    @GetMapping("parents")
     public ResponseEntity getParents(){
         return ResponseEntity.ok(dataAccessLayer.getParents());
     }
     //Review
-    @GetMapping("/get/review/{id}")
+    @GetMapping("review/{id}")
     public ResponseEntity getReviewById(@PathVariable("id") long id){
         return ResponseEntity.ok(dataAccessLayer.getReviewById(id));
     }
-    @GetMapping("/get/reviewByStudentId")
+    @GetMapping("reviewByStudentId")
     public List<Review> getReviewByStudentId(@RequestParam Long id) {
         return dataAccessLayer.getReviewByStudentId(id);
     }
-    @GetMapping("/get/reviewByTeacherId")
+    @GetMapping("reviewByTeacherId")
     public List<Review> getReviewByTeacherId(@RequestParam Long id) {
         return dataAccessLayer.getReviewByTeacherId(id);
     }
-    @GetMapping("get/reviews")
+    @GetMapping("reviews")
     public ResponseEntity getReviews(){
         return ResponseEntity.ok(dataAccessLayer.getReviews());
     }
     //Specialization
-    @GetMapping("/get/specialization/{id}")
+    @GetMapping("specialization/{id}")
     public ResponseEntity getSpecializationById(@PathVariable("id") long id){
         return ResponseEntity.ok(dataAccessLayer.getSpecializationById(id));
     }
-    @GetMapping("get/specializations")
+    @GetMapping("specializations")
     public ResponseEntity getSpecializations(){
         return ResponseEntity.ok(dataAccessLayer.getSpecializations());
     }
     //Student
-    @GetMapping("/get/student/{id}")
+    @GetMapping("student/{id}")
     public ResponseEntity getStudentById(@PathVariable("id") long id){
         return ResponseEntity.ok(dataAccessLayer.getStudentById(id));
     }
-    @GetMapping("/get/studentByGroupId")
+    @GetMapping("studentByGroupId")
     public List<Student> getStudentByGroupId(@RequestParam Long id) {
         return dataAccessLayer.getStudentByGroupId(id);
     }
-    @GetMapping("/get/studentBySpecializationId")
+    @GetMapping("studentBySpecializationId")
     public List<Student> getStudentBySpecializationId(@RequestParam Long id) {
         return dataAccessLayer.getStudentBySpecializationId(id);
     }
-    @GetMapping("get/students")
+    @GetMapping("students")
     public ResponseEntity getStudents(){
         return ResponseEntity.ok(dataAccessLayer.getStudents());
     }
     //Teacher
-    @GetMapping("/get/teacher/{id}")
+    @GetMapping("teacher/{id}")
     public ResponseEntity getTeacherById(@PathVariable("id") long id){
         return ResponseEntity.ok(dataAccessLayer.getTeacherById(id));
     }
-    @GetMapping("get/teachers")
+    @GetMapping("teachers")
     public ResponseEntity getTeachers(){
         return ResponseEntity.ok(dataAccessLayer.getTeachers());
     }
